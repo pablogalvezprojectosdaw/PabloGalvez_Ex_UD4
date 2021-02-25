264f8405fc988d03a6d813ab551ad4fa887d2928
pablogalvezprojectosdaw <77491520+pablogalvezprojectosdaw@users.noreply.github.com>
2021-02-25 14:36:07 +0100

    Revision 4. PGR-JavaDoc crearPartido.java

--- src/CrearPartido.java
+++ src/CrearPartido.java
@@ -1,7 +1,10 @@
 
 import java.awt.event.KeyEvent;
 
-
+/**
+ * 
+ * @author 1DAW03
+ */
 
 public class CrearPartido extends javax.swing.JFrame
 {
@@ -9,13 +12,18 @@ public class CrearPartido extends javax.swing.JFrame
     
     private Apuesta v;
     
-   
+   /**
+    * 
+    */
     public CrearPartido() 
     {
         initComponents();
     }
 
-   
+    /**
+     * 
+     * @param ventana 
+     */
     public CrearPartido(Apuesta ventana) 
     {
         initComponents();
@@ -69,18 +77,27 @@ public class CrearPartido extends javax.swing.JFrame
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
+    /**
+     * 
+     * @param p 
+     */
     public void añadePartido(String p)
     {
         v.partidoNuevo(p);
         tPartido.setText("");
         tPartido.requestFocus();
     }
-    
+    /**
+     * 
+     * @param evt 
+     */
     private void bAgregarPartidoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bAgregarPartidoActionPerformed
         añadePartido(tPartido.getText());
     }//GEN-LAST:event_bAgregarPartidoActionPerformed
-
+    /**
+     * 
+     * @param evt 
+     */
     private void tPartidoKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_tPartidoKeyPressed
         if( evt.getKeyCode() == KeyEvent.VK_ENTER )
         {
@@ -88,7 +105,10 @@ public class CrearPartido extends javax.swing.JFrame
         }
     }//GEN-LAST:event_tPartidoKeyPressed
 
-    
+    /**
+     * 
+     * @param args 
+     */
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
